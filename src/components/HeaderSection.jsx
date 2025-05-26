532a2a24d5ccc7d7d0ef595901eff980cd9004fd
Karo Yousefi <karoyousefi.80@gmail.com>
2025-05-26 18:22:17 +0330

    Added the modal for new habit

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 
-const HeaderSection = () => {
+const HeaderSection = ({ addModalOpen, setAddModalOpen }) => {
 	return (
 		<div className="flex justify-between items-center gap-3">
 			<div className="flex flex-col justify-center items-start gap-2">
@@ -12,7 +11,10 @@ const HeaderSection = () => {
 				</p>
 			</div>
 			<div>
-				<button className="h-10 w-36 bg-gradient-to-br from-violet-600 to-sky-600 rounded-xl text-white font-poppins cursor-pointer select-none transition-all hover:translate-y-[3px] hover:opacity-85">
+				<button
+					className="h-10 w-36 bg-gradient-to-br from-violet-600 to-sky-600 rounded-xl text-white font-poppins cursor-pointer select-none transition-all hover:translate-y-[3px] hover:opacity-85"
+					onClick={() => setAddModalOpen(!addModalOpen)}
+				>
 					<span className="pr-3">+</span>Add Habbit
 				</button>
 			</div>
