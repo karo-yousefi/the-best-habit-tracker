f162b9ee05af952e6cf9f94890dc066d65b99066
Karo Yousefi <karoyousefi.80@gmail.com>
2025-05-31 01:26:03 +0330

    Added the import / export functionality

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,10 +1,13 @@
-import { Info } from "lucide-react";
+import { Info, Database} from "lucide-react";
 import { useState } from "react";
 import InfoModal from "./InfoModal";
+import ImportExportModal from "./ImportExportModal";
 
 const HeaderSection = ({ addModalOpen, setAddModalOpen }) => {
 
 	const [infoModalOpen, setInfoModalOpen] = useState(false);
+	const [importExportModalOpen, setImportExportModalOpen] = useState(false);
+
 
 
 	return (
@@ -20,17 +23,24 @@ const HeaderSection = ({ addModalOpen, setAddModalOpen }) => {
 					Build better habits, one day at a time
 				</p>
 			</div>
-			<div>
+			<div className="flex flex-col items-center gap-3 md:flex-row-reverse">
 				<button
-					className="h-10 w-36 bg-gradient-to-br from-violet-600 to-sky-600 rounded-xl text-white font-poppins cursor-pointer select-none transition-all hover:translate-y-[3px] hover:opacity-85"
+					className="h-10 w-25 md:w-36 text-[13px] md:text-[16px] bg-gradient-to-br from-violet-600 to-sky-600 rounded-xl text-white font-poppins cursor-pointer select-none transition-all hover:translate-y-[3px] hover:opacity-85"
 					onClick={() => setAddModalOpen(!addModalOpen)}
 				>
-					<span className="pr-3">+</span>Add Habbit
+					<span className="pr-3 hidden md:inline">+</span>Add Habbit
+				</button>
+				<button
+					className="h-10 w-27 md:w-36 text-[13px] md:text-[16px] bg-gradient-to-br from-violet-600 to-sky-600 rounded-xl text-white font-poppins cursor-pointer select-none transition-all hover:translate-y-[3px] hover:opacity-85"
+					onClick={() => setImportExportModalOpen(!importExportModalOpen)}
+				>
+					Import / Export
 				</button>
 			</div>
 		</div>
 
 		<InfoModal infoModalOpen={infoModalOpen} setInfoModalOpen={setInfoModalOpen} />
+		<ImportExportModal importExportModalOpen={importExportModalOpen} setImportExportModalOpen={setImportExportModalOpen} />
 	</div>
 		
 	);
